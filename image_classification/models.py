bee3adfee79bc84ccf01c06ed80a388ae8e07f30
Qianhuang Chen <qh_chen@outlook.com>
2024-02-21 23:59:28 +0800

    fix for timm 0.6.5 and above (new args in create_model)
    Signed-off-by: Qianhuang Chen <qh_chen@outlook.com>

--- image_classification/models.py
+++ image_classification/models.py
@@ -140,7 +140,7 @@ class ResFormer(nn.Module):
     def __init__(self, img_size= (224, ), patch_size= 16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
                  num_heads=12, mlp_ratio=4., qkv_bias=True, representation_size=None, 
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0., embed_layer=PatchEmbed, norm_layer=None,
-                 act_layer=None, weight_init='', use_checkpoint = False):
+                 act_layer=None, weight_init='', use_checkpoint = False, pretrained_cfg=None, pretrained_cfg_overlay=None):
         super().__init__()
         if isinstance(img_size, int):
             img_size = (img_size)
